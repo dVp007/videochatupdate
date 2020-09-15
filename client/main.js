c1757a0224c9b023cee43d3f31c564cf3ebcae34
dvp007 <dharmilp03@gmail.com>
2020-09-15 21:47:15 +0530

    update

--- client/main.js
+++ client/main.js
@@ -4661,7 +4661,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    api_url: "https://update-video-chat.herokuapp.com/",
+    api_url: "https://update-video-chat.herokuapp.com",
 };
 /*
  * For easier debugging in development mode, you can import the following file
